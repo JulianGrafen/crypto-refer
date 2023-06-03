ae636bdfba7c8bd33e9f40d40a96bbcd28c8c7ee
JulianGrafen <juliangraefen@gmail.com>
2023-06-03 19:04:30 +0200

    Removed Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
