99fda5b490a97bbfad5d6d41712443fc5ebc02b4
JulianGrafen <juliangraefen@gmail.com>
2023-06-16 15:32:23 +0200

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      react: "https://cdn.skypack.dev/react@17",
+      "react-dom": "https://cdn.skypack.dev/react-dom@17",
+    },
+  },
   base: "/crypto-refer/",
   plugins: [react()],
   assetsInclude: ["**/*.mov"],
