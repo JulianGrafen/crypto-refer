d7cc096abae1181c1b9992391ecc8923420550bf
JulianGrafen <juliangraefen@gmail.com>
2023-06-16 18:49:23 +0200

    changed react-router.dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
-
 import App from "./App";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
