f9ddae57f8c55f65e1cdd56bd8d09aa8993ddd14
JulianGrafen <juliangraefen@gmail.com>
2023-06-16 15:11:50 +0200

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/crypto-refer/",
+  base: "/",
   plugins: [react()],
   assetsInclude: ["**/*.mov"],
   build: {
