1d0053c2d3e4403c17705a69743dece226638fd5
JulianGrafen <juliangraefen@gmail.com>
2023-06-16 18:53:52 +0200

    Changed main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import ReactDOM from "react-dom/client";
+
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
